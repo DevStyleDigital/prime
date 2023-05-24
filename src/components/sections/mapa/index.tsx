847d6e5bd2710b2a1fc1d698b25dd23d58ecb826
Afonso <84323490+AfonsoBezerra@users.noreply.github.com>
2023-05-24 19:33:54 -0300

    Update index.tsx

--- src/components/sections/mapa/index.tsx
+++ src/components/sections/mapa/index.tsx
@@ -3,7 +3,7 @@ import { MapaStyle } from "./style"
 const Mapa = () => {
     return (
         <MapaStyle>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234128.05470030542!2d-47.795408405468734!3d-23.52347829999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58b6275079c9b%3A0x54763b8fd0d6705e!2sPRIME%20MOTO%20PE%C3%87AS!5e0!3m2!1spt-BR!2sbr!4v1684758489544!5m2!1spt-BR!2sbr" style={{border:0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.2508565770117!2d-47.49514585958965!3d-23.52347823218713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58b1d295e6761%3A0x8d6539037dc62770!2sR.%20Francisco%20Paulo%20Braion%2C%2039%20-%20Jardim%20Guadalajara%2C%20Sorocaba%20-%20SP%2C%2018045-620!5e0!3m2!1spt-BR!2sbr!4v1684967570204!5m2!1spt-BR!2sbr" style={{border:0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </MapaStyle>
     )
 }
