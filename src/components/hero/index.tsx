73e54e27038ab55c94104ce0fa2d4186676bc31c
Afonso Bezerra <afonsobezerra199@gmail.com>
2024-01-10 17:28:09 -0300

    new-ofertas

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,14 +3,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper";
 import { HeroStyle } from "./style";
 import Agenda from "../../assets/img/banners/desktop/AnyConv.com__Agende o seu serviço.webp"
-import Oleo from "../../assets/img/banners/desktop/AnyConv.com__Oleo.webp"
+import Capacete from "../../assets/img/banners/desktop/Oferta-capacete.jpg"
 import Revisao from "../../assets/img/banners/desktop/AnyConv.com__Revisão da sua Moto_.webp"
-import Suporte from "../../assets/img/banners/desktop/AnyConv.com__Tudo_Para_Sua_Moto.webp"
+import Seguranca from "../../assets/img/banners/desktop/oferta-seguranca.jpg"
 import Tudo from "../../assets/img/banners/desktop/AnyConv.com__Tudo_Para_Sua_Moto.webp"
 import AgendaMobile from "../../assets/img/banners/mobile/AnyConv.com__Agende o seu serviço 400x600.webp"
-import OleoMobile from "../../assets/img/banners/mobile/AnyConv.com__Oleo400x600.webp"
+import CapaceteMobile from "../../assets/img/banners/mobile/Oferta-capacete-mobile.jpg"
 import RevisaoMobile from "../../assets/img/banners/mobile/AnyConv.com__Revisão da sua Moto 400x600.webp"
-import SuporteMobile from "../../assets/img/banners/mobile/AnyConv.com__Suporte 400x600.webp"
+import SegurancaMobile from "../../assets/img/banners/mobile/oferta-seguranca-mobile.jpg"
 import TudoMobile from "../../assets/img/banners/mobile/AnyConv.com__Tudo_Para_Sua_Moto 400x600.webp"
 
 const Hero = () => {
@@ -27,16 +27,16 @@ const Hero = () => {
                     <img src={AgendaMobile} alt="banner 1" className="mobile" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={Oleo} alt="banner 2"  className="desk" />
-                    <img src={OleoMobile} alt="banner 2"  className="mobile" />
+                    <img src={Capacete} alt="banner 2"  className="desk" />
+                    <img src={CapaceteMobile} alt="banner 2"  className="mobile" />
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={Revisao} alt="banner 3"  className="desk" />
                     <img src={RevisaoMobile} alt="banner 3"  className="mobile" />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={Suporte} alt="banner 4" className="desk" />
-                    <img src={SuporteMobile} alt="banner 4"  className="mobile" />
+                    <img src={Seguranca} alt="banner 4" className="desk" />
+                    <img src={SegurancaMobile} alt="banner 4"  className="mobile" />
                 </SwiperSlide>
                 <SwiperSlide>
                     <img src={Tudo} alt="banner 5"  className="desk" />
